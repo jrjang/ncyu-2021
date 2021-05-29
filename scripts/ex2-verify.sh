6b4d32d89cb460df1538ec0158e6a7eab40020a4
1043028 <s1043028@mail.ncyu.edu.tw>
2021-05-29 11:17:27 +0800

    [Example 2] 1043028
    Signed-off-by: 1043028 <s1043028@mail.ncyu.edu.tw>

--- scripts/ex2-verify.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-set -e
-
-if ! [[ "`git show HEAD`" =~ "`cat data/0001-ex2.patch`" ]]; then
-	echo "It is different between the requirement with your modification!"
-	exit 1
-fi
-
-exit 0
